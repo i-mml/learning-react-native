2384ba2c685ddd5ba3771f65f970826ad1409323
i_mml <mohamadja272@gmail.com>
2023-02-28 15:53:55 +0330

    handle flex box in rn

--- App.js
+++ App.js
@@ -1,28 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, TextInput, View } from 'react-native';
 
 export default function App() {
   const [text, setText] = useState("I'm gonna be changed");
   return (
     <View style={styles.container}>
-      <Text style={styles.firstText}>
-       {text}
-      </Text>
-      <Button title="Change Text" onPress={() => setText("Haha you got me!")} />
-      <StatusBar style="auto" />
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <TextInput
+          placeholder="Course Gold"
+          style={{
+            width: "80%",
+            borderColor: "black",
+            borderWidth: 1,
+            padding: 10,
+            marginBottom: 5,
+          }}
+        />
+        <Button title="ADD" color="red"/>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  firstText: {
-    color: "red",
+    padding: 50,
   },
 });
