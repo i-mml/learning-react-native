458b971a0204f3b810c5a71d1876fc997ad172ad
i_mml <mohamadja272@gmail.com>
2023-03-01 16:18:31 +0330

    handle state management for todo , styling the list

--- App.js
+++ App.js
@@ -1,12 +1,38 @@
 import { useState } from 'react';
-import { Button, StyleSheet, TextInput, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState("");
+  const [goals, setGoals] = useState([]);
+
   return (
     <View style={styles.container}>
       <View style={styles.inputContaienr}>
-        <TextInput placeholder="Course Gold" style={styles.courseInput} />
-        <Button title="ADD" color="red" />
+        <TextInput
+          placeholder="Course Goal"
+          style={styles.courseInput}
+          onChangeText={(enteredText) => setEnteredGoal(enteredText)}
+          value={enteredGoal}
+        />
+        <Button
+          title="ADD"
+          color="green"
+          onPress={() => {
+            setEnteredGoal("");
+            setGoals([...goals, enteredGoal]);
+          }}
+        />
+      </View>
+      <View style={styles.goalsListContaier}>
+        {goals?.length > 0 ? (
+          goals?.map((item) => (
+            <View key={item} style={styles?.goalItem}>
+              <Text style={{ color: "#fff" }}>{item}</Text>
+            </View>
+          ))
+        ) : (
+          <Text style={styles?.notFound}>There is no Goal to show!</Text>
+        )}
       </View>
     </View>
   );
@@ -14,11 +40,12 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    padding: 50,
+    paddingHorizontal: "10%",
+    paddingTop: 50,
   },
   inputContaienr: {
     flexDirection: "row",
-    justifyContent: "space-between",
+    justifyContent: "center",
     alignItems: "center",
   },
   courseInput: {
@@ -27,5 +54,25 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     padding: 10,
     marginBottom: 5,
+    height: "100%",
+    borderRadius: 10,
+    marginRight: 5,
+  },
+  goalsListContaier: {
+    marginTop: 10,
+    justifyContent: "center",
+  },
+  goalItem: {
+    backgroundColor: "#58a7e6",
+    marginVertical: 5,
+    padding: 10,
+    color: "#fff",
+    borderRadius: 12,
+    fontSize: 17,
+  },
+  notFound: {
+    marginTop: 10,
+    color: "#999",
+    textAlign: "center",
   },
 });
