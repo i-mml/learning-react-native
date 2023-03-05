a2ee48da21f09e5edc4990930912980471408c91
i_mml <mohamadja272@gmail.com>
2023-03-05 15:06:00 +0330

    create card component

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
+import Card from '../components/Card';
+
 const StartGameScreen = () => {
   return (
     <View style={styles.screen}>
       <Text style={styles?.title}>The Game Screen</Text>
-      <View style={styles?.inputContaienr}>
-        <Text>Select a Number</Text>
-        <TextInput placeholder="Guess a Number" />
-        <View style={styles?.buttonContaienr}>
-          <Button title="Reset" onPress={() => {}} />
-          <Button title="Confirm" onPress={() => {}} />
+      <Card>
+        <View style={styles?.inputContaienr}>
+          <Text>Select a Number</Text>
+          <TextInput placeholder="Guess a Number" />
+          <View style={styles?.buttonContaienr}>
+            <Button title="Reset" onPress={() => {}} />
+            <Button title="Confirm" onPress={() => {}} />
+          </View>
         </View>
-      </View>
+      </Card>
     </View>
   );
 };
@@ -33,10 +37,11 @@ const styles = StyleSheet.create({
     width: "100%",
     justifyContent: "space-between",
     paddingHorizontal: 15,
+    marginTop: 10,
   },
   inputContaienr: {
-    width: "80%",
-    maxWidth: 300,
+    width: 300,
+    maxWidth: "80%",
     alignItems: "center",
   },
 });
