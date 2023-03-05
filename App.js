562d05c29c65dc487c49fad8b0b453788ee01e3b
i_mml <mohamadja272@gmail.com>
2023-03-05 16:59:33 +0330

    handle game screen

--- App.js
+++ App.js
@@ -1,13 +1,28 @@
+import { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 
 import Header from './components/Header';
+import GameScreen from './screens/GameScreen';
 import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
+  const [userNumber, setUserNumber] = useState();
+
+  const startGameHandler = (number) => {
+    setUserNumber(number);
+  };
+
+  let content = <StartGameScreen onStartGame={startGameHandler} />;
+
+  if (userNumber) {
+    content = <GameScreen userChoice={userNumber} />;
+  }
+
   return (
     <View style={styles.screen}>
       <Header title="THIS IS HEADER" />
-      <StartGameScreen />
+      {content}
+      {/* <GameScreen setCurrentScreen={setCurrentScreen} /> */}
     </View>
   );
 }
