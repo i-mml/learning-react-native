9466c032a216e2126dcf88931e955705bc553d7c
i_mml <mohamadja272@gmail.com>
2023-03-11 16:12:08 +0330

    add external image

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, Image, StyleSheet, Text, View } from 'react-native';
+
+import BodyText from '../components/BodyText';
+import Colors from '../constants/Colors';
+import defaultStyles from '../constants/default-styles';
 
 const GameOverScreen = (props) => {
   const { guessRounds, userNumber, restartGame } = props;
   return (
     <View style={styles?.screen}>
-      <Text>The Game is Over!</Text>
+      <Text style={defaultStyles.title}>The Game is Over!</Text>
+      <View style={styles.imageContainer}>
+        <Image
+          fadeDuration={1000}
+          // source={require("../assets/picture1.jpg")}
+          source={{
+            uri: "https://newsmedia.tasnimnews.com/Tasnim/Uploaded/Image/1398/06/03/1398060311074941218199214.jpg",
+          }}
+          style={styles.image}
+          resizeMode="cover"
+        />
+      </View>
+      <BodyText>
+        Your phone needed{" "}
+        <Text style={styles.guessRound}>{props.guessRounds}</Text> round to
+        guess the number
+      </BodyText>
       <View style={styles?.text}>
         <Text>Number of rounds: {guessRounds}</Text>
       </View>
@@ -13,7 +33,7 @@ const GameOverScreen = (props) => {
         <Text>Number was: {userNumber}</Text>
       </View>
       <View style={styles?.button}>
-        <Button title="NEW GAME" onPress={() => restartGame()} color="black"/>
+        <Button title="NEW GAME" onPress={() => restartGame()} color="black" />
       </View>
     </View>
   );
@@ -26,11 +46,25 @@ const styles = StyleSheet.create({
     height: "75%",
     justifyContent: "center",
   },
+  imageContainer: {
+    width: 200,
+    height: 200,
+    borderRadius: 150,
+    marginVertical: 5,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    borderRadius: 150,
+  },
   text: {
     marginVertical: 10,
   },
   button: {
     marginTop: 20,
+  },
+  guessRound: {
+    color: Colors.scondary,
   },
 });
 
