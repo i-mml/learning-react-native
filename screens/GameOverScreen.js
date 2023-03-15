ba69309d85b12f8cc13b13b6bdb6bd9222cda1a0
i_mml <mohamadja272@gmail.com>
2023-03-15 11:10:16 +0330

    add scroll view to game over screen

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Dimensions, Image, StyleSheet, Text, View } from 'react-native';
+import { Dimensions, Image, ScrollView, StyleSheet, Text, View } from 'react-native';
 
 import BodyText from '../components/BodyText';
 import MainButton from '../components/MainButton';
@@ -9,38 +9,40 @@ import defaultStyles from '../constants/default-styles';
 const GameOverScreen = (props) => {
   const { guessRounds, userNumber, restartGame } = props;
   return (
-    <View style={styles?.screen}>
-      <Text style={defaultStyles.title}>The Game is Over!</Text>
-      <View style={styles.imageContainer}>
-        <Image
-          fadeDuration={200}
-          // source={require("../assets/picture1.jpg")}
-          source={{
-            uri: "https://newsmedia.tasnimnews.com/Tasnim/Uploaded/Image/1398/06/03/1398060311074941218199214.jpg",
-          }}
-          style={styles.image}
-          resizeMode="cover"
-        />
+    <ScrollView>
+      <View style={styles?.screen}>
+        <Text style={defaultStyles.title}>The Game is Over!</Text>
+        <View style={styles.imageContainer}>
+          <Image
+            fadeDuration={200}
+            // source={require("../assets/picture1.jpg")}
+            source={{
+              uri: "https://newsmedia.tasnimnews.com/Tasnim/Uploaded/Image/1398/06/03/1398060311074941218199214.jpg",
+            }}
+            style={styles.image}
+            resizeMode="cover"
+          />
+        </View>
+        <BodyText>
+          Your phone needed{" "}
+          <Text style={styles.guessRound}>{props.guessRounds}</Text> round to
+          guess the number
+        </BodyText>
+        <View style={styles?.text}>
+          <Text>Number of rounds: {guessRounds}</Text>
+        </View>
+        <View style={styles?.text}>
+          <Text>Number was: {userNumber}</Text>
+        </View>
+        <View style={styles?.button}>
+          <MainButton
+            title="NEW GAME"
+            onClick={() => restartGame()}
+            color="black"
+          />
+        </View>
       </View>
-      <BodyText>
-        Your phone needed{" "}
-        <Text style={styles.guessRound}>{props.guessRounds}</Text> round to
-        guess the number
-      </BodyText>
-      <View style={styles?.text}>
-        <Text>Number of rounds: {guessRounds}</Text>
-      </View>
-      <View style={styles?.text}>
-        <Text>Number was: {userNumber}</Text>
-      </View>
-      <View style={styles?.button}>
-        <MainButton
-          title="NEW GAME"
-          onClick={() => restartGame()}
-          color="black"
-        />
-      </View>
-    </View>
+    </ScrollView>
   );
 };
 
@@ -48,7 +50,7 @@ const styles = StyleSheet.create({
   screen: {
     padding: 10,
     alignItems: "center",
-    height: "75%",
+    // height: "75%",
     justifyContent: "center",
   },
   imageContainer: {
