41240a6cb52c8080cd42b53f1eb96619eadf7a72
i_mml <mohamadja272@gmail.com>
2023-03-27 14:09:58 +0430

    handle fetch fonts in app.js

--- App.js
+++ App.js
@@ -1,8 +1,28 @@
 import { StyleSheet, Text, View } from "react-native";
-import * as Font from "expo-font"
- 
+import * as Font from "expo-font";
+import AppLoading from "expo-app-loading";
+import { useState } from "react";
+// import { AppLoading } from "expo-app-loading";
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    "iran-sans": require("./assets/fonts/iranSans/ttf/IRANSansWeb(FaNum)_Medium.ttf"),
+    "iran-sans-bold": require("./assets/fonts/iranSans/ttf/IRANSansWeb(FaNum)_Bold.ttf"),
+  });
+};
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+        onError={(error) => console.log(error)}
+      />
+    );
+  }
   return (
     <View style={styles.screen}>
       <Text>This is the App file</Text>
