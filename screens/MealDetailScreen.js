65073a80aab55b57a6e8fd33c885c544f2f7f5a0
i_mml <mohamadja272@gmail.com>
2023-04-17 18:01:29 +0430

    go to top handler

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -6,6 +6,13 @@ const MealDetailScreen = (props) => {
     <View style={styles.screen}>
       <Text>The MealDetail Screen!</Text>
       <Button title="Go Back" onPress={() => props.navigation.goBack()} />
+      {/* navigation.popToTop is used to go to top of navigation (just in stack navigation) */}
+      <Button
+        title="Go To Categories"
+        onPress={() => {
+          props.navigation.popToTop();
+        }}
+      />
     </View>
   );
 };
