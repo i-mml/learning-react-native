8c57d6c6dd8accb9fd1fd7e13d16f330aec04f7e
i_mml <mohamadja272@gmail.com>
2023-04-19 13:27:25 +0430

    wiop handle categories list

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,19 +1,32 @@
 import React from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, FlatList, StyleSheet, Text, View } from "react-native";
+import { CATEGORIES } from "../data/dummy-data";
+import { TouchableOpacity } from "react-native";
 
 const CategoriesScreen = (props) => {
-  console.log(props);
-  return (
-    <View style={styles.screen}>
-      <Text>The Categories Screen!</Text>
-      <Button
-        title="Go to Meals!"
+  const renderGridItem = (itemData) => {
+    return (
+      <TouchableOpacity
         onPress={() => {
           // when we want to go to the current screen we use navigation.push("ROUTE_NAME")
           props?.navigation.navigate("CategoryMeals");
         }}
-      />
-    </View>
+        style={styles?.gridItem}
+      >
+        <View>
+          <Text>{itemData?.item?.title}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
+  return (
+    <FlatList
+      numColumns={2}
+      renderItem={renderGridItem}
+      data={CATEGORIES}
+      keyExtractor={(item) => item?.id}
+    />
   );
 };
 
@@ -22,6 +35,11 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  gridItem: {
+    flex: 1,
+    margin: 15,
+    height: 150,
   },
 });
 
