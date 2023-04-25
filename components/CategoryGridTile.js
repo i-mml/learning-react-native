04ce9c03b9a8254162292d0e875244be4b259d76
i_mml <mohamadja272@gmail.com>
2023-04-25 12:13:59 +0430

    add shadow to grid item

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -42,7 +42,11 @@ const styles = StyleSheet.create({
     margin: 15,
     height: 150,
     borderRadius: 10,
-    overflow: "hidden",
+    overflow:
+      Platform.OS === "android" && Platform?.Version >= 21
+        ? "hidden"
+        : "visible",
+    elevation: 5,
   },
   container: {
     flex: 1,
@@ -51,7 +55,6 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.26,
     shadowOffset: { width: 1, height: 2 },
     shadowRadius: 10,
-    elevation: 3,
     padding: 15,
     justifyContent: "flex-end",
     alignItems: "flex-end",
