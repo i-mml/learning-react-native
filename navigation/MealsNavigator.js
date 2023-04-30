882f9c6803ba188a60c99b17a68703b794c3637b
i_mml <mohamadja272@gmail.com>
2023-04-30 10:53:17 +0430

    handle tab bar component and icons

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -3,19 +3,123 @@
 import CategoriesScreen from "../screens/CategoriesScreen";
 import CategoryMealsScreen from "../screens/CategoryMealsScreen";
 import MealDetailScreen from "../screens/MealDetailScreen";
-import { Platform } from "react-native";
+import { Platform, Text, TouchableOpacity, View } from "react-native";
 import Colors from "../constants/Colors";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import FavoritesScreen from "../screens/FavoritesScreen";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
+import { Ionicons } from "@expo/vector-icons";
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
+const MyTabBar = ({ state, descriptors, navigation }) => {
+  return (
+    <View
+      style={{
+        width: "100%",
+        position: "absolute",
+        bottom: 0,
+        flexDirection: "row",
+        // height: 30,
+        backgroundColor: "white",
+      }}
+    >
+      {state.routes.map((route, index) => {
+        const { options } = descriptors[route.key];
+
+        const label =
+          options.tabBarLabel !== undefined
+            ? options.tabBarLabel
+            : options.title !== undefined
+            ? options.title
+            : route.name;
+
+        const isFocused = state.index === index;
+
+        const onPress = () => {
+          const event = navigation.emit({
+            type: "tabPress",
+            target: route.key,
+            canPreventDefault: true,
+          });
+
+          if (!isFocused && !event.defaultPrevented) {
+            // The `merge: true` option makes sure that the params inside the tab screen are preserved
+            navigation.navigate({ name: route.name, merge: true });
+          }
+        };
+
+        const onLongPress = () => {
+          navigation.emit({
+            type: "tabLongPress",
+            target: route.key,
+          });
+        };
+
+        return (
+          <TouchableOpacity
+            accessibilityRole="button"
+            accessibilityState={isFocused ? { selected: true } : {}}
+            accessibilityLabel={options.tabBarAccessibilityLabel}
+            testID={options.tabBarTestID}
+            onPress={onPress}
+            onLongPress={onLongPress}
+            style={{
+              flex: 1,
+              width: "50%",
+              backgroundColor:
+                isFocused && Platform.OS === "android"
+                  ? Colors.primaryColor
+                  : "white",
+            }}
+            key={index}
+          >
+            <View style={{ alignItems: "center", justifyContent: "center" }}>
+              <Text>
+                <Ionicons
+                  name={options?.tabBarIcon}
+                  style={{
+                    color:
+                      isFocused && Platform.OS === "android"
+                        ? "white"
+                        : Colors.primaryColor,
+                  }}
+                  size={25}
+                />
+              </Text>
+            </View>
+            <Text
+              style={{
+                color:
+                  isFocused && Platform.OS === "android"
+                    ? "white"
+                    : Colors.primaryColor,
+                textAlign: "center",
+              }}
+            >
+              {label}
+            </Text>
+          </TouchableOpacity>
+        );
+      })}
+    </View>
+  );
+};
+
 const MealsNavigator2 = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor:
+            Platform.OS === "android" ? Colors.primaryColor : "white",
+        },
+        headerTintColor:
+          Platform.OS === "android" ? "white" : Colors.primaryColor,
+      }}
+    >
       <Stack.Screen name="Categories" component={CategoriesScreen} />
       <Stack.Screen name="CategoryMeals" component={CategoryMealsScreen} />
       <Stack.Screen name="MealDetail" component={MealDetailScreen} />
@@ -26,13 +130,28 @@ const MealsNavigator2 = () => {
 export default function MealsFavTabNavigator() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator tabBar={(props) => <MyTabBar {...props} />}>
         <Tab.Screen
           name="CategoriesNested"
           component={MealsNavigator2}
-          options={{ headerShown: false }}
+          options={{
+            headerShown: false,
+            tabBarIcon: "ios-restaurant",
+          }}
         />
-        <Tab.Screen name="Favorites" component={FavoritesScreen} />
+        <Tab.Screen
+          name="Favorites"
+          component={FavoritesScreen}
+          options={{
+            headerStyle: {
+              backgroundColor:
+                Platform.OS === "android" ? Colors.primaryColor : "white",
+            },
+            headerTintColor:
+              Platform.OS === "android" ? "white" : Colors.primaryColor,
+            tabBarIcon: "bookmark-outline",
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
