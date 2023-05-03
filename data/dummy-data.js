73cd1c4862a96cac84543e91f706f0a4357d9c7b
i_mml <mohamadja272@gmail.com>
2023-05-03 13:39:12 +0430

    wip handle redux and product's dymmy data

---
+++ data/dummy-data.js
@@ -0,0 +1,52 @@
+import Product from "../models/product";
+
+export const PRODUCTS = [
+  new Product(
+    "p1",
+    "u1",
+    "Red Shirt",
+    "https://19sa.ir/wp-content/uploads/2020/07/Pirahan_004_1.jpg",
+    "A red shirt , perfect for days with non-red weather.",
+    29.99
+  ),
+  new Product(
+    "p2",
+    "u1",
+    "Blue Shirt",
+    "https://cdn.modiseh.com/unsafe/orig/aj2877_1.jpg",
+    "First your red shirt perfectly. To stand on . not to wear it.",
+    31.5
+  ),
+  new Product(
+    "p3",
+    "u2",
+    "jeans Shirt",
+    "https://dkstatics-public.digikala.com/digikala-products/117618986.jpg?x-oss-process=image/resize,m_lfit,h_800,w_800/quality,q_90",
+    "Jeans are awsome for active people , if you are a guy who has active life , then do not hesitate to buy it.",
+    43.85
+  ),
+  new Product(
+    "p4",
+    "u2",
+    "Ladies Black Handy Bag",
+    "https://shaniland.com/uploads/products/img-20230313-213818-1678730898vUHsSbU2c7bNKLDn.jpg",
+    "Black Handy Bag with a lot of space for Ladies stuffs",
+    16.4
+  ),
+  new Product(
+    "p5",
+    "u1",
+    "Ladies Gray Handy Bag",
+    "https://shaniland.com/uploads/products/img-20230313-213835-1678730915raBuW6FvE71xWI6S.jpg",
+    "Gray Handy Bag with a lot of space for Ladies stuffs",
+    16.2
+  ),
+  new Product(
+    "p6",
+    "u3",
+    "Men's Brown Handy Bag",
+    "https://charmbarez.com/wp-content/uploads/2022/02/44-1.jpg",
+    "Brown Handy Bag with a lot of space for Men's stuffs",
+    24.99
+  ),
+];
---
+++ screens/shop/productsOverviewScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { FlatList } from "react-native";
+import { View } from "react-native";
+
+const productsOverviewScreen = () => {
+  return (
+    <View>
+      <FlatList data={[]} />
+    </View>
+  );
+};
+
+export default productsOverviewScreen;
