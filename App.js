f52ba95e831c924cf1776288b282890903afd2c7
i_mml <mohamadja272@gmail.com>
2023-05-07 11:02:35 +0430

    add fonts in updated way

--- App.js
+++ App.js
@@ -1,32 +1,28 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import ShopNavigator from "./navigation/ShopNavigator";
-import * as Font from "expo-font";
-import AppLoading from "expo-app-loading";
 import { useState } from "react";
+import AppLoading from "expo-app-loading";
+import { useFonts } from "expo-font";
 import { enableScreens } from "react-native-screens";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
+import { useCallback } from "react";
 
 // for improve performance in navigations
 enableScreens();
 
-const fetchFonts = () => {
-  return Font.loadAsync({
+export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+  const [fontsLoaded] = useFonts({
     "iran-sans": require("./assets/fonts/iranSans/ttf/IRANSansWeb(FaNum)_Medium.ttf"),
     "iran-sans-bold": require("./assets/fonts/iranSans/ttf/IRANSansWeb(FaNum)_Bold.ttf"),
   });
-};
 
-export default function App() {
-  const [fontLoaded, setFontLoaded] = useState(false);
-
-  if (!fontLoaded) {
+  if (!fontsLoaded) {
     return (
-      <AppLoading
-        startAsync={fetchFonts}
-        onFinish={() => setFontLoaded(true)}
-        onError={(error) => console.log(error)}
-      />
+      <View>
+        <Text>Wait to fetch fonts</Text>
+      </View>
     );
   }
   return (
